cf5330919741c3f37a88f34a3a6dbf0b379df927
wifon <kaziorigor0902@gmail.com>
2026-03-23 15:43:56 +0100

    6.3 Zmiana magicznej liczby na stałą CAPACITY

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -20,7 +20,7 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == CAPACITY - 1;
     }
 
     protected int peekaboo() {
