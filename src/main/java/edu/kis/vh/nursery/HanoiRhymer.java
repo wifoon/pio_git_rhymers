e921c099be9defc68238eeb1e63dd33366479c84
wifon <kaziorigor0902@gmail.com>
2026-03-23 15:22:01 +0100

    12. Usunięcie nieużywanych setterów

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -19,8 +19,4 @@ public class HanoiRhymer extends DefaultCountingOutRhymer {
     public int getTotalRejected() {
         return totalRejected;
     }
-
-    public void setTotalRejected(int totalRejected) {
-        this.totalRejected = totalRejected;
-    }
 }
