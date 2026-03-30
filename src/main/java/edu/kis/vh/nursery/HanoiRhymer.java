04ec54f7fad36b2f2696c1d83f9acb29d2a4ae84
wifon <kaziorigor0902@gmail.com>
2026-03-30 13:30:28 +0200

    17. Dodanie dokumentacji dla klasy HanoiRhymer

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,13 +1,31 @@
 package edu.kis.vh.nursery;
 
+/**
+ * Klasa HanoiRhymer uniemożliwia ona dodanie liczby większej niż ta,
+ * która aktualnie znajduje się na szczycie wyliczanki.
+ */
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
     private static final int REJECTED = 0;
+    /**
+     * Licznik odrzuconych elementów.
+     */
     private int totalRejected = REJECTED;
 
+    /**
+     * Zwraca całkowitą liczbę odrzuconych elementów.
+     * @return liczba odrzuconych elementów.
+     */
     public int reportRejected() {
         return totalRejected;
     }
+
+    /**
+     * Dodaje nową wartość do wyliczanki. Jeżeli nowa wartość jest większa
+     * od aktualnej wartości szczytowej, operacja jest przerywana,
+     * a licznik odrzuceń zostaje zwiększony.
+     * @param in wartość całkowita, która ma zostać dodana.
+     */
     @Override
     public void countIn(int in) {
         if (!callCheck() && in > peekaboo())
@@ -16,6 +34,10 @@ public class HanoiRhymer extends DefaultCountingOutRhymer {
             super.countIn(in);
     }
 
+    /**
+     * Getter dla pola totalRejected.
+     * @return aktualna wartość licznika odrzuceń.
+     */
     public int getTotalRejected() {
         return totalRejected;
     }
